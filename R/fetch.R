895ee9642bd52cae5955e5714ec141fc6a561386
Yihui Xie <xie@yihui.name>
2017-04-12 22:18:15 -0500

    use a different arrow

--- R/fetch.R
+++ R/fetch.R
@@ -20,7 +20,7 @@ for (i in seq_len(NROW(m))) {
   }
   s$text = gsub('\\s*$', '', s$text)
   s$text = gsub('\n\n', '\n>\n', s$text)
-  s$text = paste(s$text, sprintf(' [&#8631;](https://twitter.com/xieyihui/status/%s)', s$status_id))
+  s$text = paste(s$text, sprintf(' [&#8618;](https://twitter.com/xieyihui/status/%s)', s$status_id))
   x = c(
     x, paste('#', q), '',
     paste0('> **', u$name, '** (@', u$screen_name, '; ', s$favorite_count, '/',
