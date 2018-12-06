6d8fc5c28622cf900a050008d586f9c746b7315e
Yihui Xie <xie@yihui.name>
2018-12-06 09:40:48 -0600

    only show the first word in h1

--- R/fetch.R
+++ R/fetch.R
@@ -28,7 +28,7 @@ for (i in seq_len(NROW(m))) {
   s$text = gsub('\n\n', '\n>\n', s$text)
   s$text = paste(s$text, sprintf(' [&#8618;](https://twitter.com/xieyihui/status/%s)', s$status_id))
   x = c(
-    x, paste('#', q), '',
+    x, paste('#', gsub(' .+', '', q)), '',
     paste0('> **', u$name, '** (@', u$screen_name, '; ', s$favorite_count, '/',
            s$retweet_count, '): ', s$text, '\n\n<!-- -->\n\n')
   )
